ed23a2515c842f82d8c721786d871fbf33d18e65
gledsong2 <40277875+gledsong2@users.noreply.github.com>
2018-08-07 21:54:19 -0300

    criacao do select na tela resumo

---
+++ Desktop/Java/Redoma/src/model/bean/IndicesNoPrimary.java
@@ -0,0 +1,61 @@
+
+package model.bean;
+
+public class IndicesNoPrimary {
+    private String nomeDaTabela;
+    private String nomeDoIndice;
+    private long idDoObjeto;
+    private String grupoDeArquivo;
+    private String tipoDeIndice;
+    private String tipoDeTabela;
+
+    public String getNomeDaTabela() {
+        return nomeDaTabela;
+    }
+
+    public void setNomeDaTabela(String nomeDaTabela) {
+        this.nomeDaTabela = nomeDaTabela;
+    }
+
+    public String getNomeDoIndice() {
+        return nomeDoIndice;
+    }
+
+    public void setNomeDoIndice(String nomeDoIndice) {
+        this.nomeDoIndice = nomeDoIndice;
+    }
+
+    public long getIdDoObjeto() {
+        return idDoObjeto;
+    }
+
+    public void setIdDoObjeto(long idDoObjeto) {
+        this.idDoObjeto = idDoObjeto;
+    }
+
+    public String getGrupoDeArquivo() {
+        return grupoDeArquivo;
+    }
+
+    public void setGrupoDeArquivo(String grupoDeArquivo) {
+        this.grupoDeArquivo = grupoDeArquivo;
+    }
+
+    public String getTipoDeIndice() {
+        return tipoDeIndice;
+    }
+
+    public void setTipoDeIndice(String tipoDeIndice) {
+        this.tipoDeIndice = tipoDeIndice;
+    }
+
+    public String getTipoDeTabela() {
+        return tipoDeTabela;
+    }
+
+    public void setTipoDeTabela(String tipoDeTabela) {
+        this.tipoDeTabela = tipoDeTabela;
+    }
+    
+    
+}
